7d9a81b5a1af0929a65af554fd1a83a2d7ddc81f
Deb <deborayemina@gmail.com>
2024-02-09 02:15:42 -0300

    1

--- obj/Debug/net7.0/NotasApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/NotasApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotasApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7934c6b7f575fe3301aa9d97b0233017956901ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65755f55e0b970ff475f9afd00bbc4ba071b5b1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotasApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotasApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
